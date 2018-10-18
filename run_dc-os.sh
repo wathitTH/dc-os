1dc3ca7e1f530356e892fb99c1d84d26b77c290d
wathitTH <42666666+wathitTH@users.noreply.github.com>
2018-10-18 13:56:27 +0700

    Update run_dc-os.sh
    re network

--- run_dc-os.sh
+++ run_dc-os.sh
@@ -3,7 +3,7 @@ docker run -dt \
 --name=dc-os \
 --hostname dc-os \
 --privileged \
---network cloudera --ip=177.10.10.8 \
+--network traceoncloud --ip=177.10.10.8 \
 -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
 -v /root/dcos-storage/hosts:/etc/hosts \
 -v /root/dcos-storage/forward.traceoncloud:/var/named/forward.traceoncloud \
@@ -12,6 +12,6 @@ docker run -dt \
 -v /root/dcos-storage/named.conf:/etc/named.conf \
 -v /root/dcos-storage/kerberos:/root/var/kerberos \
 -v /root/dcos-storage/krb5.conf:/etc/krb5.conf \
-wathit/dcos:v2
+wathit/dc-os:v1
 docker ps | grep dc-os
 docker exec -it dc-os /bin/bash
